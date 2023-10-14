1c7fcb4c2896aa62e025d8ea801b64849213328c
Mushbt <mustafa_habet@hotmail.com>
2023-10-14 09:16:38 +0000

    Commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreetings';
 
 function App() {
   return (
     <div className="App">
       <StatefulGreeting greeting="I'm a stateful class component!" />
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" />
     </div>
   );
 }
